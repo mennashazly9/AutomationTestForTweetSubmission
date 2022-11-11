5f751801db518410f606a64cbc9b2fb30545119d
mennashazly <mennashazly22@gmail.com>
2022-11-11 03:05:34 +0200

    webdriver handler implementation

---
+++ src/test/java/com/twitter/driver/WebDriverHandler.java
@@ -0,0 +1,40 @@
+package com.twitter.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WebDriverHandler {
+    private static WebDriver webDriver;
+
+    private static final String chromeDriverPath = "src/test/java/driverExtensions/chromedriver";
+
+
+    public WebDriverHandler() {
+        setWebDriver();
+    }
+
+    public static WebDriver getWebDriver() {
+        return webDriver;
+    }
+
+    private void setWebDriver() {
+        String chromeDriverType = "webdriver.chrome.driver";
+        System.setProperty(chromeDriverType, chromeDriverPath);
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+
+    }
+
+    public void navigateTo(String link) {
+        webDriver.navigate().to(link);
+    }
+
+    public void resetCache() {
+        webDriver.manage().deleteAllCookies();
+    }
+
+    public static void close() {
+        webDriver.quit();
+    }
+}
